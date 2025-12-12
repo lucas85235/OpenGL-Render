007cd35f3f78ed4d34de4d901e5f64b1b888e789
Lucas Lima <lucas85235@gmail.com>
2025-12-12 10:51:21 -0400

    Fix materials

--- src/rhi/vulkan/vulkan_device.cpp
+++ src/rhi/vulkan/vulkan_device.cpp
@@ -886,17 +886,21 @@ void VulkanDevice::createUniformBuffers() {
 }
 
 void VulkanDevice::createDescriptorPool() {
+  // Increase pool sizes to support multiple textures per frame
+  constexpr uint32_t MAX_SAMPLERS = 100;
+
   std::array<VkDescriptorPoolSize, 2> poolSizes{};
   poolSizes[0].type = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
   poolSizes[0].descriptorCount = static_cast<uint32_t>(MAX_FRAMES_IN_FLIGHT);
   poolSizes[1].type = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
-  poolSizes[1].descriptorCount = static_cast<uint32_t>(MAX_FRAMES_IN_FLIGHT);
+  poolSizes[1].descriptorCount = MAX_SAMPLERS;
 
   VkDescriptorPoolCreateInfo poolInfo{};
   poolInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_POOL_CREATE_INFO;
+  poolInfo.flags = VK_DESCRIPTOR_POOL_CREATE_FREE_DESCRIPTOR_SET_BIT;
   poolInfo.poolSizeCount = static_cast<uint32_t>(poolSizes.size());
   poolInfo.pPoolSizes = poolSizes.data();
-  poolInfo.maxSets = static_cast<uint32_t>(MAX_FRAMES_IN_FLIGHT);
+  poolInfo.maxSets = MAX_SAMPLERS;
 
   if (vkCreateDescriptorPool(device, &poolInfo, nullptr, &descriptorPool) !=
       VK_SUCCESS) {
